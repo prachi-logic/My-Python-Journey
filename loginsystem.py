86ef322ad827f06fcd3bbf8fa8a9e48114bf9615
prachi agrawal <agrawalprachi860@gmail.com>
2026-05-27 11:21:57 +0530

    Added loginsystem.py

---
+++ loginsystem.py
@@ -0,0 +1,8 @@
+user=input('Enter username:')
+password=input("Enter password:")
+user_name='prachi'
+password_user='10001'
+if user == user_name and password == password_user:
+    print("you are ready to login")
+else:
+    print("you are wrong")
